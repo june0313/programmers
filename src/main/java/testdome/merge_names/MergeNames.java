c97dfafb656e9c9c6dba316e71f09b7b2bad1db1
Young Jun Park <june0313@gmail.com>
2020-02-24 21:49:14 +0900

    [testdome] MergeNames 문제 풀이

---
+++ src/main/java/testdome/merge_names/MergeNames.java
@@ -0,0 +1,19 @@
+package testdome.merge_names;
+
+import java.util.*;
+
+public class MergeNames {
+
+    public static String[] uniqueNames(String[] names1, String[] names2) {
+        Set<String> set = new HashSet<>();
+        set.addAll(Arrays.asList(names1));
+        set.addAll(Arrays.asList(names2));
+        return set.toArray(new String[]{});
+    }
+
+    public static void main(String[] args) {
+        String[] names1 = new String[] {"Ava", "Emma", "Olivia"};
+        String[] names2 = new String[] {"Olivia", "Sophia", "Emma"};
+        System.out.println(String.join(", ", MergeNames.uniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+    }
+}
