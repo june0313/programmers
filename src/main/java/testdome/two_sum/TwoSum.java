0e00c313c8787ab7ddb0f587da675e5c12ad95aa
Young Jun Park <june0313@gmail.com>
2020-02-26 06:29:51 +0900

    [testdome] Two sum 문제 풀이 - HashMap 활용

--- src/main/java/testdome/two_sum/TwoSum.java
+++ src/main/java/testdome/two_sum/TwoSum.java
@@ -1,20 +1,27 @@
 package testdome.two_sum;
 
+import java.util.*;
+
 public class TwoSum {
     public static int[] findTwoSum(int[] list, int sum) {
-        for (int i = 0; i < list.length - 1; i++) {
-            for (int k = i + 1; k < list.length; k++) {
-                if (list[i] + list[k] == sum) {
-                    return new int[]{i, k};
-                }
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < list.length; i++) {
+            int target = sum - list[i];
+
+            if (map.containsKey(target)) {
+                return new int[]{i, map.get(target)};
+            } else {
+                map.put(list[i], i);
             }
         }
+
         return null;
     }
 
     public static void main(String[] args) {
-        int[] indices = findTwoSum(new int[] { 3, 1, 5, 7, 5, 9 }, 10);
-        if(indices != null) {
+        int[] indices = findTwoSum(new int[]{5, 5, 5, 7, 5, 9}, 10);
+        if (indices != null) {
             System.out.println(indices[0] + " " + indices[1]);
         }
     }
