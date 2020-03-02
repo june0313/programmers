010ce77e4ea0357c7d20435da01a91a2a938d000
Young Jun Park <june0313@gmail.com>
2020-03-02 22:16:40 +0900

    [programmers] 전화번호 목록 문제 풀이 (HashSet 활용)

--- src/main/java/programmers/highscorekit/level_2/전화번호목록/Prefix.java
+++ src/main/java/programmers/highscorekit/level_2/전화번호목록/Prefix.java
@@ -1,18 +1,19 @@
 package programmers.highscorekit.level_2.전화번호목록;
 
+import java.util.*;
+
 public class Prefix {
     public boolean solution(String[] phoneBook) {
-        for (int i = 0; i < phoneBook.length; i++) {
-            for (int k = 0; k < phoneBook.length; k++) {
-                if ( i == k) {
-                    continue;
-                }
+        Set<String> set = new HashSet<>(Arrays.asList(phoneBook));
 
-                if (phoneBook[i].startsWith(phoneBook[k])) {
+        for (String phoneNumber : phoneBook) {
+            for (int i = 0; i < phoneNumber.length(); i++) {
+                if (set.contains(phoneNumber.substring(0, i))) {
                     return false;
                 }
             }
         }
+
         return true;
     }
 }
