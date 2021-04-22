743167c0fba86312455d4a436e63119e5c3bf6e9
Young Jun Park <june0313@gmail.com>
2021-04-22 10:42:51 +0900

    [programmers] 삼각 달팽이 - 더 좋은 풀이법

--- src/main/java/programmers/level_2/삼각달팽이/TriangleSnail.java
+++ src/main/java/programmers/level_2/삼각달팽이/TriangleSnail.java
@@ -43,4 +43,39 @@ public class TriangleSnail {
 
         return Arrays.stream(triangle).flatMapToInt(Arrays::stream).toArray();
     }
+
+    /**
+     * 세로 -> 가로 -> 대각선 -> 세로 -> 가로 -> 대각선... 의 형태로 반복이 된다.
+     * 이 때 채워야 하는 각 칸의 개수가 n 부터 0 까지 하나씩 줄어든다.
+     * ex) n = 6, 세로(6) -> 가로(5) -> 대각선(4) -> 세로(3) -> 가로(2) -> 대각선(1) -> 세로(0)
+     * 전체 n 번 반복 회차에 대한 인덱스를 i라고 하면 i를 3으로 나누었을때 나머지 값을 보면 어떤 방향으로 채워야 하는지 판단할 수 있다.
+     *      n % 3 == 0 -> 세로
+     *      n % 3 == 1 -> 가로
+     *      n % 3 == 2 -> 대각선
+     */
+    public int[] solution2(int n) {
+        int[][] triangle = IntStream.rangeClosed(1, n).mapToObj(int[]::new).toArray(int[][]::new);
+
+        int y = -1;
+        int x = 0;
+        int number = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (i % 3 == 0) {
+                    y++;
+                }
+                if (i % 3 == 1) {
+                    x++;
+                }
+                if (i % 3 == 2) {
+                    y--;
+                    x--;
+                }
+                triangle[y][x] = number++;
+            }
+        }
+
+        return Arrays.stream(triangle).flatMapToInt(Arrays::stream).filter(i -> i > 0).toArray();
+    }
 }
--- src/test/java/programmers/level_2/삼각달팽이/TriangleSnailTest.java
+++ src/test/java/programmers/level_2/삼각달팽이/TriangleSnailTest.java
@@ -22,4 +22,14 @@ public class TriangleSnailTest {
         assertArrayEquals(sut.solution(5), new int[]{1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9});
         assertArrayEquals(sut.solution(6), new int[]{1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11});
     }
+
+    @Test
+    public void test2() {
+        assertArrayEquals(sut.solution2(1), new int[]{1});
+        assertArrayEquals(sut.solution2(2), new int[]{1, 2, 3});
+        assertArrayEquals(sut.solution2(3), new int[]{1, 2, 6, 3, 4, 5});
+        assertArrayEquals(sut.solution2(4), new int[]{1, 2, 9, 3, 10, 8, 4, 5, 6, 7});
+        assertArrayEquals(sut.solution2(5), new int[]{1, 2, 12, 3, 13, 11, 4, 14, 15, 10, 5, 6, 7, 8, 9});
+        assertArrayEquals(sut.solution2(6), new int[]{1, 2, 15, 3, 16, 14, 4, 17, 21, 13, 5, 18, 19, 20, 12, 6, 7, 8, 9, 10, 11});
+    }
 }
